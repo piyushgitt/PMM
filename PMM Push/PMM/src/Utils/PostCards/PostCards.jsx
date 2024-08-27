5486ed39d070add28feb5e64e9e62ad600b7fb35
Piyush Prasoon <piyushpr5679@gmail.com>
2024-08-27 12:30:37 +0530

    frontend of myPosts page

--- PMM Push/PMM/src/Utils/PostCards/PostCards.jsx
+++ PMM Push/PMM/src/Utils/PostCards/PostCards.jsx
@@ -5,14 +5,17 @@ import { faHandPointer , faComment } from '@fortawesome/free-solid-svg-icons';
 import { useSiso } from '../../Context/siso';
 import LikePrompts from '../LikePrompts/LikePrompts';
 import CommentsOn from "../CommentsOn/CommentsOn";
+import AddComments from '../AddComments/AddComments';
 
 
 const PostCards = ({ bgcs , bgheader , post  }) => {
 const siso = useSiso();
 const [isLiked, setIsLiked] = useState(post.likedBy.includes(siso.userInfo.email));
 const [noOfPushes , setNoOfPushes] = useState(post.Likes);
+const [noOfComments , setNoOfComments] = useState(post.Comments.length);
 const [showLikedBy , setShowLikedBy] = useState(false);
 const [showComments, setShowComments] = useState(false);
+const [showAddComments, setShowAddComments] = useState(false);
 
   const handleLikes = () =>{
     if(!isLiked){
@@ -32,80 +35,103 @@ const [showComments, setShowComments] = useState(false);
     }
     else siso.clearLikedBy();
   },[showLikedBy]);
- 
+
+
+  useEffect(() => {
+    if(showComments){
+     siso.getComments(post.emailId, post.postId);
+    }
+    else siso.clearComments();
+  },[showComments])
+
 
   return (
     <>
-      {showLikedBy && (
-        <div className="LikedByPrompt">
-          {" "}
-          <LikePrompts post={post} />{" "}
-        </div>
-      )}
-      <div className="upmain">
-        <div className="upc" style={{ background: `${bgcs}` }}>
-          <div className="upctop" style={{ background: `${bgheader}` }}>
-            <div className="upctopprofilepic">
+      <div className="likepostcomment">
+        {showLikedBy && (
+          <div className="LikedByPrompt">
+            {" "}
+            <LikePrompts post={post} />{" "}
+          </div>
+        )}
+        <div className="upmain">
+          <div className="upc" style={{ background: `${bgcs}` }}>
+            <div className="upctop" style={{ background: `${bgheader}` }}>
+              <div className="upctopprofilepic">
                 <img
                   className="upctopprofilepicimg"
                   src={post.profilePic}
                   alt="img"
                   srcset=""
                 />
+              </div>
+              <div className="upctopnameandsubtitle">
+                <div className="upctopname">{post.fullName}</div>
+                <div className="upctopsubtitle">5 posts, 2 accomplishments</div>
+              </div>
             </div>
-            <div className="upctopnameandsubtitle">
-              <div className="upctopname">{post.fullName}</div>
-              <div className="upctopsubtitle">5 posts, 2 accomplishments</div>
+            <div className="upcbottom" style={{ background: `${bgcs}` }}>
+              {post.Motive}
             </div>
           </div>
-          <div className="upcbottom" style={{ background: `${bgcs}` }}>
-            {post.Motive}
+          <div className="uppushcomment" style={{ background: `${bgcs}` }}>
+            <div className="uppush" style={{ background: `${bgcs}` }}>
+              <div
+                className={`upicon ${isLiked ? " liked" : ""}`}
+                onClick={handleLikes}
+              >
+                <FontAwesomeIcon icon={faHandPointer} />
+              </div>
+              <div
+                className="uppushtext"
+                onClick={async () => {
+                  await siso.clearLikedBy();
+                  setShowComments(false);
+                  setShowAddComments(false);
+                  setShowLikedBy(!showLikedBy);
+                }}
+              >
+                {noOfPushes} {noOfPushes == 1 ? `push` : `pushes`}
+              </div>
+            </div>
+            <div className="upcomment" style={{ background: `${bgcs}` }}>
+              <div className="upcommenticon">
+                <FontAwesomeIcon
+                  className="commenticon"
+                  icon={faComment}
+                  style={{ color: "green" }}
+                  onClick={() => {
+                    setShowComments(false);
+                    setShowLikedBy(false);
+                    setShowAddComments(!showAddComments);
+                  }}
+                />
+              </div>
+              <div
+                className="upcommenttext"
+                onClick={() => {
+                  setShowLikedBy(false);
+                  setShowAddComments(false);
+                  setShowComments(!showComments);
+                }}
+              >
+                {noOfComments} Comments
+              </div>
+            </div>
           </div>
         </div>
-        <div className="uppushcomment" style={{ background: `${bgcs}` }}>
-          <div className="uppush" style={{ background: `${bgcs}` }}>
-            <div
-              className={`upicon ${isLiked ? " liked" : ""}`}
-              onClick={handleLikes}
-            >
-              <FontAwesomeIcon icon={faHandPointer} />
-            </div>
-            <div
-              className="uppushtext"
-              onClick={async () => {
-                await siso.clearLikedBy();
-                setShowComments(false);
-                setShowLikedBy(!showLikedBy);
-              }}
-            >
-              {noOfPushes} {noOfPushes == 1 ? `push` : `pushes`}
-            </div>
+        {showAddComments && (
+          <div className="upAddComments">
+            <AddComments post={post} />
           </div>
-          <div className="upcomment" style={{ background: `${bgcs}` }}>
-            <div className="upcommenticon">
-              <FontAwesomeIcon
-                className="commenticon"
-                icon={faComment}
-                style={{ color: "green" }}
-              />
-            </div>
-            <div
-              className="upcommenttext"
-              onClick={() => {
-                setShowLikedBy(false);
-                setShowComments(!showComments);
-              }}
-            >
-              4 comments
-            </div>
+        )}
+        {showComments && (
+          <div className="upcomments">
+            {" "}
+            <CommentsOn />{" "}
           </div>
-        </div>
+        )}
       </div>
-      {showComments && (
-        <div className="upcomments">
-          <CommentsOn />
-        </div>
-      )}
     </>
   );
 };
