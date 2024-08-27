c215c6709e2a3c7740c9d9eb5dada8af0222b1e3
Piyush Prasoon <piyushpr5679@gmail.com>
2024-08-27 12:09:17 +0530

    Starting work on myposts page

--- PMM Push/PMM/src/Context/siso.jsx
+++ PMM Push/PMM/src/Context/siso.jsx
@@ -27,7 +27,12 @@ import {
   onSnapshot,
 } from "firebase/firestore";
 import { Navigate, useNavigate } from "react-router-dom";
-import { getStorage , ref , uploadBytesResumable , getDownloadURL } from "firebase/storage";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBOVcvCwpIeAj3zS2TIrKBdXI_g5bA4SMs",
@@ -62,12 +67,13 @@ export const SisoProvider = (props) => {
   const [allFriendsPosts, setAllFriendsPosts] = useState([]); // all friends post of the user;
   const [usersLiked, setUsersLiked] = useState([]);
   const [refreshFeed, setRefreshFeed] = useState(false);
+  const [allComments, setAllComments] = useState([]);
 
   //adding user name and email to his database
   const add_user = async ({ ...userData }) => {
     try {
-       const date = new Date().getTime();
-       const storageRef = ref(storage, `${userData.email + date}`);
+      const date = new Date().getTime();
+      const storageRef = ref(storage, `${userData.email + date}`);
       const userDocRef = doc(userInfodb, "users", userData.email);
       await uploadBytesResumable(storageRef, userData.profilePic).then(() => {
         getDownloadURL(storageRef).then(async (downloadURL) => {
@@ -79,7 +85,6 @@ export const SisoProvider = (props) => {
           });
         });
       });
-      
     } catch (error) {
       alert("Error while adding document");
     }
@@ -92,6 +97,13 @@ export const SisoProvider = (props) => {
     await sign_up_user(data.email, data.password);
     add_user(data);
     await sign_in_user(data.email, data.password);
+  };
+
+  //geting user photo
+  const getUserPhoto = async (userId) => {
+    const docRef = doc(userInfodb, "users", userId);
+    const theDocRef = await getDoc(docRef);
+    return theDocRef.data().profilePic;
   };
 
   //handling login and logout
@@ -115,17 +127,13 @@ export const SisoProvider = (props) => {
   const sign_up_user = async (email, password) => {
     setPersistence(appAuth, browserSessionPersistence).then(async () => {
       try {
-        await createUserWithEmailAndPassword(appAuth, email, password).then(
-          () => {
+        await createUserWithEmailAndPassword(appAuth, email, password)
+          .then(() => {
             sendEmailVerification(appAuth.currentUser).then(() => {
               alert("Please verify your email via a link sent on your email");
             });
-          }
-        ).then(
-          () => {
-
-          }
-        );
+          })
+          .then(() => {});
       } catch (error) {
         alert("Enter valid email address");
       }
@@ -197,7 +205,7 @@ export const SisoProvider = (props) => {
         postId: postId,
         fullName: userInfo.first_name + " " + userInfo.last_name,
         likedBy: [],
-        profilePic : userInfo.profilePic,
+        profilePic: userInfo.profilePic,
       });
       console.log("Success");
     } catch (err) {
@@ -260,11 +268,15 @@ export const SisoProvider = (props) => {
         thePost.data().likedBy.forEach(async (likedEmail, index) => {
           const docRef = doc(userInfodb, "users", likedEmail);
           const userData = await getDoc(docRef);
-
+          const profilePic = await getUserPhoto(likedEmail);
           setUsersLiked((prev) => {
             return [
               ...prev,
-              userData.data().first_name + " " + userData.data().last_name,
+              {
+                fullName:
+                  userData.data().first_name + " " + userData.data().last_name,
+                profilePic: profilePic,
+              },
             ];
           });
         });
@@ -296,6 +308,54 @@ export const SisoProvider = (props) => {
   const clearLikedBy = () => {
     setUsersLiked([]);
   };
+
+  //add comment code
+  const addComment = async (userId, postId, cmntObj) => {
+    const thePostRef = doc(userInfodb, "users", userId, "userMotives", postId);
+    const thePost = await getDoc(thePostRef);
+    var newComments = thePost.data().Comments;
+    newComments = [...newComments, cmntObj];
+    await updateDoc(thePostRef, {
+      Comments: newComments,
+    });
+    console.log("success");
+  };
+
+  //get comment  code
+  const getComments = async (userId, postId) => {
+    const unsub = onSnapshot(
+      doc(userInfodb, "users", userId, "userMotives", postId),
+      (thePost) => {
+        setAllComments([]);
+        thePost.data().Comments.forEach(async (Comment, index) => {
+          const docRef = doc(userInfodb, "users", Comment.commentedBy);
+          const userData = await getDoc(docRef);
+          setAllComments((prev) => {
+            return [
+              ...prev,
+              {
+                fullName:
+                  userData.data().first_name + " " + userData.data().last_name,
+                date: Comment.date,
+                Comment: Comment.comment,
+                profilePic: userData.data().profilePic,
+              },
+            ];
+          });
+        });
+      }
+    );
+    // console.log(allComments);
+    return () => {
+      unsub();
+    };
+  };
+
+  //clear comments;
+  const clearComments = () => {
+    setAllComments([]);
+  };
+
   return (
     <SisoContext.Provider
       value={{
@@ -313,6 +373,10 @@ export const SisoProvider = (props) => {
         usersLiked,
         clearLikedBy,
         likedUsers,
+        addComment,
+        getComments,
+        allComments,
+        clearComments,
       }}
     >
       {props.children}
